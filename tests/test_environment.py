e55e61a419b865e0b0a9f0dc89c28a34b666635b
Business-Data-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2023-05-09 21:40:04 -0600

    tests: simplify py vers check

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,25 +1,9 @@
 import sys
 
-REQUIRED_PYTHON = "python3"
 
+def test_required_python_version():
+    py_major: int = sys.version_info.major
+    py_minor: int = sys.version_info.minor
+    sys.version_info
 
-def main():
-    system_major = sys.version_info.major
-    if REQUIRED_PYTHON == "python":
-        required_major = 2
-    elif REQUIRED_PYTHON == "python3":
-        required_major = 3
-    else:
-        raise ValueError("Unrecognized python interpreter: {}".format(
-            REQUIRED_PYTHON))
-
-    if system_major != required_major:
-        raise TypeError(
-            "This project requires Python {}. Found: Python {}".format(
-                required_major, sys.version))
-    else:
-        print(">>> Development environment passes all tests!")
-
-
-if __name__ == '__main__':
-    main()
+    assert py_major == 3 and py_minor >= 9
